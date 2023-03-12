1334a4a59864eef27dd8e220b2b4f2c724feafea
Sahil Sharma <46827078+sahil-at-home1@users.noreply.github.com>
2023-03-11 17:33:58 -0800

    refactored texture code out to texture module

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ use winit::{
 use rand::Rng;
 use bytemuck;
 
+mod texture;
+
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 struct Vertex {
@@ -82,6 +84,7 @@ struct State {
     index_buffer_idx: usize,
     num_indices: u32,
     diffuse_bind_group: wgpu::BindGroup,
+    diffuse_texture: texture::Texture,
 }
 
 impl State {
@@ -132,54 +135,7 @@ impl State {
         surface.configure(&device, &config);
         // get the image file as bytes
         let diffuse_bytes = include_bytes!("happy-tree.png");
-        let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
-        let diffuse_rgba = diffuse_image.to_rgba8();
-        use image::GenericImageView;
-        let dimensions = diffuse_image.dimensions();
-        // create the texture
-        let texture_size = wgpu::Extent3d {
-            width: dimensions.0,
-            height: dimensions.1,
-            depth_or_array_layers: 1, // make the texture 2D instead of 3D
-        };
-        let texture_desc = wgpu::TextureDescriptor {
-            size: texture_size,
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format: wgpu::TextureFormat::Rgba8UnormSrgb,
-            // texture binding to bind to shader
-            // copy_dst to copy data to the texture
-            usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
-            label: Some("Diffuse Texture"),
-            view_formats: &[],
-        };
-        let diffuse_texture = device.create_texture(&texture_desc);
-        // get data into the texture
-        let image_copy_location = wgpu::ImageCopyTexture {
-            texture: &diffuse_texture,
-            mip_level: 0,
-            origin: wgpu::Origin3d::ZERO,
-            aspect: wgpu::TextureAspect::All,
-        };
-        let image_data_layout = wgpu::ImageDataLayout {
-            offset: 0,
-            bytes_per_row: std::num::NonZeroU32::new(4 * dimensions.0),
-            rows_per_image: std::num::NonZeroU32::new(dimensions.1),
-        };
-        queue.write_texture(image_copy_location, &diffuse_rgba, image_data_layout, texture_size);
-        // define texture sampler (how to get a color from texture)
-        let diffuse_texture_view = diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());
-        let diffuse_sampler_desc = wgpu::SamplerDescriptor {
-            address_mode_u: wgpu::AddressMode::ClampToEdge,
-            address_mode_v: wgpu::AddressMode::ClampToEdge,
-            address_mode_w: wgpu::AddressMode::ClampToEdge,
-            mag_filter: wgpu::FilterMode::Linear,
-            min_filter: wgpu::FilterMode::Nearest,
-            mipmap_filter: wgpu::FilterMode::Nearest,
-            ..Default::default()
-        };
-        let diffuse_sampler = device.create_sampler(&diffuse_sampler_desc);
+        let diffuse_texture = texture::Texture::from_bytes(&device, &queue, diffuse_bytes, "happy-tree.png").unwrap();
         // create bind group to describe how textures can be accessed by shader
         let sampled_texture = wgpu::BindGroupLayoutEntry {
             binding: 0,
@@ -204,11 +160,11 @@ impl State {
         let texture_bind_group_layout = device.create_bind_group_layout(&bind_group_layout_desc);
         let bind_group_entry1 = wgpu::BindGroupEntry {
             binding: 0,
-            resource: wgpu::BindingResource::TextureView(&diffuse_texture_view),
+            resource: wgpu::BindingResource::TextureView(&diffuse_texture.view),
         };
         let bind_group_entry2 = wgpu::BindGroupEntry {
             binding: 1,
-            resource: wgpu::BindingResource::Sampler(&diffuse_sampler),
+            resource: wgpu::BindingResource::Sampler(&diffuse_texture.sampler),
         };
         let bind_group_desc = wgpu::BindGroupDescriptor {
             layout: &texture_bind_group_layout,
@@ -348,6 +304,7 @@ impl State {
             index_buffer_idx,
             num_indices,
             diffuse_bind_group,
+            diffuse_texture,
         }
     }
 
