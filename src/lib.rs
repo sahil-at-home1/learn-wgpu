4d58da37d66470bf135cefa029da6c53eeddf447
Sahil Sharma <46827078+sahil-at-home1@users.noreply.github.com>
2023-03-11 16:52:05 -0800

    updated vertex and fragment shaders to work with textures

--- src/lib.rs
+++ src/lib.rs
@@ -12,21 +12,21 @@ use bytemuck;
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 struct Vertex {
     position: [f32; 3],
-    color: [f32; 3],
+    tex_coords: [f32; 2],
 }
 
 impl Vertex {
     fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
         let attributes = &[
-            wgpu::VertexAttribute {
-                    offset: 0,
-                    shader_location: 0,
-                    format: wgpu::VertexFormat::Float32x3,
+            wgpu::VertexAttribute { // position
+                offset: 0,
+                shader_location: 0,
+                format: wgpu::VertexFormat::Float32x3,
             },
-            wgpu::VertexAttribute {
+            wgpu::VertexAttribute { // tex coords
                 offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
                 shader_location: 1,
-                format: wgpu::VertexFormat::Float32x3,
+                format: wgpu::VertexFormat::Float32x2,
             },
         ];
         let vertex_buffer_layout = wgpu::VertexBufferLayout {
@@ -39,16 +39,16 @@ impl Vertex {
 }
 
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [-0.50, -0.75, 0.0], color: [0.5, 0.0, 0.5] }, 
-    Vertex { position: [0.50, -0.75, 0.0], color: [0.5, 0.0, 0.5] }, 
-    Vertex { position: [0.75, 0.50, 0.0], color: [0.5, 0.0, 0.5] }, 
-    Vertex { position: [0.00, 1.00, 0.0], color: [0.5, 0.0, 0.5] },
-    Vertex { position: [-0.75, 0.50, 0.0], color: [0.5, 0.0, 0.5] },
-    Vertex { position: [-0.30, 0.00, 0.0], color: [0.5, 0.0, 0.5] },
-    Vertex { position: [0.00, -0.30, 0.0], color: [0.5, 0.0, 0.5] },
-    Vertex { position: [0.30, 0.00, 0.0], color: [0.5, 0.0, 0.5] },
-    Vertex { position: [0.25, 0.45, 0.0], color: [0.5, 0.0, 0.5] },
-    Vertex { position: [-0.25, 0.45, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [-0.50, -0.75, 0.0], tex_coords: [0.4131759, 0.99240386], }, 
+    Vertex { position: [0.50, -0.75, 0.0],  tex_coords: [0.0048659444, 0.56958647], }, 
+    Vertex { position: [0.75, 0.50, 0.0],   tex_coords: [0.28081453, 0.05060294], }, 
+    Vertex { position: [0.00, 1.00, 0.0],   tex_coords: [0.85967, 0.1526709], },
+    Vertex { position: [-0.75, 0.50, 0.0],  tex_coords: [0.9414737, 0.7347359], },
+    Vertex { position: [-0.30, 0.00, 0.0],  tex_coords: [0.4131759, 0.99240386], },
+    Vertex { position: [0.00, -0.30, 0.0],  tex_coords: [0.0048659444, 0.56958647], },
+    Vertex { position: [0.30, 0.00, 0.0],   tex_coords: [0.28081453, 0.05060294], },
+    Vertex { position: [0.25, 0.45, 0.0],   tex_coords: [0.85967, 0.1526709], },
+    Vertex { position: [-0.25, 0.45, 0.0],  tex_coords: [0.9414737, 0.7347359], },
 ];
 
 // triangles have their vertices arranged in counter-clockwise order
@@ -234,7 +234,7 @@ impl State {
         // create render pipeline
         let render_pipeline_layout_desc = wgpu::PipelineLayoutDescriptor{
             label: Some("Render Pipeline Layout"),
-            bind_group_layouts: &[],
+            bind_group_layouts: &[&texture_bind_group_layout],
             push_constant_ranges: &[],
         };
         let render_pipeline_layout = device.create_pipeline_layout(&render_pipeline_layout_desc);
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -2,32 +2,39 @@
 
 struct VertexInput {
     @location(0) position: vec3<f32>,
-    @location(1) color: vec3<f32>,
+    @location(1) tex_coords: vec2<f32>,
 };
 
 struct VertexOutput {
     @builtin(position) clip_position: vec4<f32>,
-    @location(0) color: vec3<f32>,
+    @location(0) tex_coords: vec2<f32>,
 };
 
 @vertex
 fn vs_main(model: VertexInput) -> VertexOutput {
     var out: VertexOutput;
-    out.color = model.color;
+    out.tex_coords = model.tex_coords;
     out.clip_position = vec4<f32>(model.position, 1.0);
     return out;
 }
 
 // Fragment shader
+@group(0) @binding(0) // from 1st parameter in set_bind_group()
+var t_diffuse: texture_2d<f32>;
+@group(0) @binding(1) // from binding specified in BindGroupLayout and BindGroup
+var s_diffuse: sampler;
+
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-    // set the color
-    return vec4<f32>(in.color, 1.0);
+    // set the color by sampling texture
+    return textureSample(t_diffuse, s_diffuse, in.tex_coords);
 }
 
 // Fragment shader 2
 @fragment
 fn fs_main2(in: VertexOutput) -> @location(0) vec4<f32> {
+    // set the color by sampling texture
+    return textureSample(t_diffuse, s_diffuse, in.tex_coords);
     // set the color
-    return vec4<f32>(in.clip_position.x, in.clip_position.y, in.clip_position.z, 1.0);
+    // return vec4<f32>(in.clip_position.x, in.clip_position.y, in.clip_position.z, 1.0);
 }
